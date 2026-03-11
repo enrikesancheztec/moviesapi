878bd3fb534b61eb7535d7d1948da8b60317348c
Enrique Sanchez <enrike.sanchez@gmail.com>
2026-03-11 17:48:59 -0600

    Tests fixed

--- src/test/java/com/kikesoft/moviesapi/controller/MoviesControllerIntegrationTests.java
+++ src/test/java/com/kikesoft/moviesapi/controller/MoviesControllerIntegrationTests.java
@@ -2,11 +2,13 @@ package com.kikesoft.moviesapi.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@WebMvcTest(MoviesController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class MoviesControllerIntegrationTests {
     @Autowired
     private MockMvc mockMvc;
