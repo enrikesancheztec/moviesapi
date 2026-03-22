a69e2d1d901409778c1d6814ede5edcda9f01fa4
Enrique Sanchez <enrike.sanchez@gmail.com>
2026-03-22 13:05:25 -0600

    issue-6 refactor MoviesService to delegate data access to MoviesDAO and remove hardcoded movie values

--- src/main/java/com/kikesoft/moviesapi/service/MoviesService.java
+++ src/main/java/com/kikesoft/moviesapi/service/MoviesService.java
@@ -1,46 +1,24 @@
 package com.kikesoft.moviesapi.service;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.kikesoft.moviesapi.enumeration.Rating;
+import com.kikesoft.moviesapi.dao.MoviesDAO;
 import com.kikesoft.moviesapi.vo.MovieVO;
 
 @Service
 public class MoviesService {
+    @Autowired
+    private MoviesDAO moviesDAO;
+
     public Optional<MovieVO> findById(Long id) {
-        if (id == 1) {
-            MovieVO movie = new MovieVO(1L,
-                    "Star Wars: Episode IV - A New Hope",
-                    LocalDate.of(1977, 5, 25),
-                    121,
-                    Rating.PG,
-                    "Luke Skywalker begins his journey as a Jedi Knight...");
-            return Optional.of(movie);
-        } else {
-            return Optional.empty();
-        }
+        return moviesDAO.findById(id);
     }
 
     public List<MovieVO> findAll() {
-        List<MovieVO> movies = List.of(
-                new MovieVO(1L,
-                        "Star Wars: Episode IV - A New Hope",
-                        LocalDate.of(1977, 5, 25),
-                        121,
-                        Rating.PG,
-                        "Luke Skywalker begins his journey as a Jedi Knight..."),
-                new MovieVO(2L,
-                        "The Godfather",
-                        LocalDate.of(1972, 3, 24),
-                        175,
-                        Rating.R,
-                        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son...")
-                    );
-        // Implementation for finding all movies
-        return movies;
+        return moviesDAO.findAll();
     }
 }
